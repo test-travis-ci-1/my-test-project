bb3856bc5ff2e90f15fa090a0727b18736d37af8
test-travis-ci-1 <test.travis-ci.1@yandex.ru>
2018-06-13 12:19:18 +0300

    #3

---
+++ tests/2.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Spec 2', () => {
+    it('test 1', () => {
+        expect(1).to.eql(1);
+    });
+
+    it('test 2', () => {
+        expect(1).to.eql(1);
+    });
+});
